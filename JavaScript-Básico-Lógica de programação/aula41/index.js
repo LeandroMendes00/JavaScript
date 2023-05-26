88ecc33e07b8992b1f735668a67ba33f70822152
Leandro <91228406+lmendes2001@users.noreply.github.com>
2023-05-25 21:34:44 -0300

    Curso de javascript

---
+++ JavaScript-Básico-Lógica de programação/aula41/index.js
@@ -0,0 +1,12 @@
+// Escreva uma função que recebe 2 números e torne o maior deles
+// retorne o maior deles
+const max2 = (x, y) => x > y ? x : y; 
+console.log(max2(10, 5));
+//    if (x > y) {
+//        return x;
+//    } else {
+//        return y;
+//    }
+// const max2 = (x, y) => {
+//     return x > y ? x : y;
+// };
